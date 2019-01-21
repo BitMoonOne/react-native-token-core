859c1350639afeab8af83b385b512118b1272592
wlg <wangligong_job@163.com>
2019-01-21 14:32:57 +0800

    update

--- src/main/java/com/wallet/core/wallet/WalletManager.java
+++ src/main/java/com/wallet/core/wallet/WalletManager.java
@@ -1,5 +1,6 @@
 package com.wallet.core.wallet;
 
+import com.wallet.core.wallet.address.EthereumAddressCreator;
 import com.wallet.core.wallet.model.ChainType;
 import com.wallet.core.wallet.model.EthAccount;
 import com.wallet.core.wallet.model.Wallet;
@@ -21,6 +22,55 @@ public class WalletManager {
         //TODO 1.加载wallet.info文件2判断wallet.info 是否有内容 有按照wallet.info初始化钱包 没有，初始化钱包，并在wallet.info中记录账户的索引
         //TODO 2.按照wallet.info 初始化钱包，根据当前索引，从0开始加载addr信息（eth）
         //TODO 3.从0开始加载addr，并保存到文件里面
+
+        //1加载wallet.info
+        //-    [eth:{index:1}]
+        String wallet_info = "wallet_info";
+        String walletInfo = wallet_info;
+
+        if(walletInfo != null && !"".equals(walletInfo)){
+
+            List<String> words = getWordsFromWalletInfo(walletInfo);
+
+            //加载ETH
+            Integer index_eth = getIndexFromWalletInfo(walletInfo, ChainType.ETHEREUM);
+            //获取addr
+
+            EthereumAddressCreator ethGreat = new EthereumAddressCreator();
+            //["0x", "0x"]
+            List<String> addrListEth = new ArrayList<>();
+            for (int i = 0; i <= index_eth; i++) {
+                String addr = ethGreat.fromMnemonic(words, i);
+                addrListEth.add(addr);
+            }
+
+            //ower public key
+            // user public key  其他钱包都一样
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+    private static List<String> getWordsFromWalletInfo(String walletInfo) {
+        return new ArrayList<String>();
+    }
+
+    private static Integer getIndexFromWalletInfo(String info, String chainType) {
+        return 0;
     }
 
     /**
